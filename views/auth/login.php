8dce82c59b39df0736ba963f3ae35415218da01a
Yoky Ben Stevanus <juarakeduasabar@gmail.com>
2024-04-25 13:08:19 +0700

    Change to Skote themes

--- views/auth/login.php
+++ views/auth/login.php
@@ -3,83 +3,142 @@
  * @var $this yii\web\View
  * @var $model ybsisgood\modules\UserManagement\models\forms\LoginForm
  */
+$this->title = 'Login';
 
-use ybsisgood\modules\UserManagement\components\GhostHtml;
-use ybsisgood\modules\UserManagement\UserManagementModule;
-use yii\bootstrap5\ActiveForm;
-use yii\helpers\Html;
+use yii\helpers\Url;
+
+$baseUrl = Url::base();
 ?>
 
-<div class="container" id="login-wrapper">
-	<div class="row justify-content-md-center">
-		<div class="col col-md-4 col-md-offset-4">
-			<div class="card panel">
-				<div class="card-header">
-					<?= Yii::$app->params['panelTitle'] ?? UserManagementModule::t('front', 'Authorization') ?>
-				</div>
-				<div class="card-body">
+<div class="account-pages my-5 pt-sm-5">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8 col-lg-6 col-xl-5">
+                <div class="card overflow-hidden">
+                    <div class="bg-primary-subtle">
+                        <div class="row">
+                            <div class="col-7">
+                                <div class="text-primary p-4">
+                                    <h5 class="text-primary">Welcome Back !</h5>
+                                    <p>Sign in to continue.</p>
+                                </div>
+                            </div>
+                            <div class="col-5 align-self-end">
+                                <img src="<?= $baseUrl; ?>/themes/skote/assets/images/profile-img.png" alt="" class="img-fluid">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body pt-0"> 
+                        <div class="auth-logo">
+                            <a href="javascript:void(0);" onclick="window.location.reload(true);" class="auth-logo-light">
+                                <div class="avatar-md profile-user-wid mb-4">
+                                    <span class="avatar-title rounded-circle bg-light">
+                                        <img src="<?= $baseUrl; ?>/themes/skote/assets/images/logo-light.svg" alt="" class="rounded-circle" height="34">
+                                    </span>
+                                </div>
+                            </a>
 
-					<?php $form = ActiveForm::begin([
-						'id'      => 'login-form',
-						'options'=>['autocomplete'=>'off'],
-						'validateOnBlur'=>false,
-						'fieldConfig' => [
-							'template'=>"{input}\n{error}",
-						],
-					]) ?>
+                            <a href="javascript:void(0);" onclick="window.location.reload(true);" class="auth-logo-dark">
+                                <div class="avatar-md profile-user-wid mb-4">
+                                    <span class="avatar-title rounded-circle bg-light">
+                                        <img src="<?= $baseUrl; ?>/themes/skote/assets/images/logo.svg" alt="" class="rounded-circle" height="34">
+                                    </span>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="p-2">
+                            <form class="form-horizontal" action="<?php echo Url::toRoute(['site/login']); ?>" method="post">
+                                <input type="hidden" name="_csrf" value="<?= Yii::$app->request->getCsrfToken() ?>" />
+                                <div class="mb-3 <?= $model->hasErrors('username') ? 'has-error' : ''; ?>">
+                                    <label for="loginform-username" class="form-label">Username</label>
+                                    <input type="text" class="form-control" id="loginform-username" name="LoginForm[username]" placeholder="Enter username">
+                                    <span class="text-danger"><?= $model->getFirstError('username') ? $model->getFirstError('username') : ''; ?></span>
+                                </div>
+        
+                                <div class="mb-3 <?= $model->hasErrors('password') ? 'has-error' : ''; ?>">
+                                    <label class="form-label">Password</label>
+                                    <div class="input-group auth-pass-inputgroup">
+                                        <input type="password" class="form-control" placeholder="Enter password" name="LoginForm[password]" aria-label="Password" aria-describedby="password-addon">
+                                        <button class="btn btn-light " type="button" id="password-addon"><i class="mdi mdi-eye-outline"></i></button>
+                                        
+                                    </div>
+                                    <span class="text-danger"><?= $model->getFirstError('password') ? $model->getFirstError('password') : ''; ?></span>
+                                </div>
 
-					<?= $form->field($model, 'username')
-						->textInput(['placeholder'=>$model->getAttributeLabel('username'), 'autocomplete'=>'off']) ?>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" id="loginform-rememberme" name="LoginForm[rememberMe]">
+                                    <label class="form-check-label" for="loginform-rememberme">
+                                        Remember me
+                                    </label>
+                                </div>
+                                
+                                <div class="mt-3 d-grid">
+                                    <button class="btn btn-primary waves-effect waves-light" type="submit">Log In</button>
+                                </div>
+    
+                                <!-- <div class="mt-4 text-center">
+                                    <h5 class="font-size-14 mb-3">Sign in with</h5>
+    
+                                    <ul class="list-inline">
+                                        <li class="list-inline-item">
+                                            <a href="javascript::void()" class="social-list-item bg-primary text-white border-primary">
+                                                <i class="mdi mdi-facebook"></i>
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item">
+                                            <a href="javascript::void()" class="social-list-item bg-info text-white border-info">
+                                                <i class="mdi mdi-twitter"></i>
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item">
+                                            <a href="javascript::void()" class="social-list-item bg-danger text-white border-danger">
+                                                <i class="mdi mdi-google"></i>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div> -->
 
-					<?= $form->field($model, 'password')
-						->passwordInput(['placeholder'=>$model->getAttributeLabel('password'), 'autocomplete'=>'off']) ?>
+                                <!-- <div class="mt-4 text-center">
+                                    <a href="#" class="text-muted"><i class="mdi mdi-lock me-1"></i> Forgot your password?</a>
+                                </div> -->
+                            </form>
+                        </div>
+    
+                    </div>
+                </div>
+                <div class="mt-5 text-center">
+                    
+                    <div>
+                        <p><?= Yii::$app->params['yearCopyright'] . ' © ' . Yii::$app->params['companyName'];?>. Crafted with <i class="mdi mdi-coffee text-danger"></i> by Juara Kedua</p>
+                    </div>
+                </div>
 
-					<?= (isset(Yii::$app->user->enableAutoLogin) && Yii::$app->user->enableAutoLogin) ? $form->field($model, 'rememberMe')->checkbox(['value'=>true]) : '' ?>
-
-					<?= Html::submitButton(
-						UserManagementModule::t('front', 'Login'),
-						['class' => 'btn btn-lg btn-primary btn-block']
-					) ?>
-
-					<div class="row registration-block">
-						<div class="col-sm-6">
-							<?= GhostHtml::a(
-								UserManagementModule::t('front', "Registration"),
-								['/user-management/auth/registration']
-							) ?>
-						</div>
-						<div class="col-sm-6 text-right">
-							<?= GhostHtml::a(
-								UserManagementModule::t('front', "Forgot password ?"),
-								['/user-management/auth/password-recovery']
-							) ?>
-						</div>
-					</div>
-					<?php ActiveForm::end() ?>
-				</div>
-			</div>
-		</div>
-	</div>
+            </div>
+        </div>
+    </div>
 </div>
 
-<?php
-$css = <<<CSS
-html, body {
-	background: #eee;
-	-webkit-box-shadow: inset 0 0 100px rgba(0,0,0,.5);
-	box-shadow: inset 0 0 100px rgba(0,0,0,.5);
-	height: 100%;
-	min-height: 100%;
-	position: relative;
-}
-#login-wrapper {
-	position: relative;
-	top: 30%;
-}
-#login-wrapper .registration-block {
-	margin-top: 15px;
-}
-CSS;
+<?php 
+$scriptThemes = <<<JS
 
-$this->registerCss($css);
+(function ($) {
+    'use strict';
+    function initPassword() {
+        // show password input value
+        $("#password-addon").on('click', function () {
+            if ($(this).siblings('input').length > 0) {
+                $(this).siblings('input').attr('type') == "password" ? $(this).siblings('input').attr('type', 'input') : $(this).siblings('input').attr('type', 'password');
+            }
+        })
+    }
+
+    function init() {
+        initPassword();
+    }
+
+    init();
+})(jQuery);
+
+JS;
+$this->registerJs($scriptThemes, yii\web\View::POS_END);
 ?>
--- views/layouts/loginLayout.php
+++ views/layouts/loginLayout.php
@@ -1,31 +1,84 @@
 <?php
+
+/** @var yii\web\View $this */
+/** @var string $content */
+
 use app\assets\AppAsset;
-use ybsisgood\modules\UserManagement\UserManagementModule;
-use yii\bootstrap5\BootstrapAsset;
 use yii\helpers\Html;
+use yii\helpers\Url;
 
-/* @var $this \yii\web\View */
-/* @var $content string */
+$baseUrl = Url::base();
 
-$this->title = Yii::$app->params['panelTitle'] ?? UserManagementModule::t('front', 'Authorization');
-BootstrapAsset::register($this);
+AppAsset::register($this);
+
+$this->registerCsrfMetaTags();
+$this->registerMetaTag(['charset' => Yii::$app->charset], 'charset');
+$this->registerMetaTag(['name' => 'viewport', 'content' => 'width=device-width, initial-scale=1.0']);
+$this->registerMetaTag(['name' => 'description', 'content' => $this->params['meta_description'] ?? '']);
+$this->registerMetaTag(['name' => 'keywords', 'content' => $this->params['meta_keywords'] ?? '']);
+$this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii::getAlias('@web/themes/icon/favicon.ico')]);
+$this->registerMetaTag(['name' => 'robots', 'content' => Yii::$app->params['robotTxt'] ?? '']);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>">
+<html lang="<?= Yii::$app->language ?>" data-layout-width="boxed" data-topbar="dark" data-preloader="disable" data-card-layout="borderless" data-bs-theme="light" data-topbar-image="pattern-1">
 <head>
-	<meta charset="<?= Yii::$app->charset ?>"/>
-	<meta name="robots" content="noindex, nofollow">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<?= Html::csrfMetaTags() ?>
-	<title><?= Html::encode($this->title) ?></title>
-	<?php $this->head() ?>
+    <title><?= Yii::$app->params['name'] ?> <?= Html::encode($this->title) ? ' | ' . Html::encode($this->title) : ''  ?></title>
+    <link href="<?= $baseUrl;?>/themes/skote/assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <link href="<?= $baseUrl;?>/themes/skote/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="<?= $baseUrl;?>/themes/skote/assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+    <script>
+        !function ($) {
+            "use strict";
+            if (window.sessionStorage) {
+                var alreadyVisited = sessionStorage.getItem("is_visited");
+                if (alreadyVisited) {
+                    switch (alreadyVisited) {
+                        case "light-mode-switch":
+                            document.documentElement.removeAttribute("dir");
+                            if (document.getElementById("bootstrap-style").getAttribute("href") != "<?= $baseUrl;?>/themes/skote/assets/css/bootstrap.min.css")
+                                document.getElementById("bootstrap-style").setAttribute("href", "<?= $baseUrl;?>/themes/skote/assets/css/bootstrap.min.css");
+                            if (document.getElementById("app-style").getAttribute("href") != "<?= $baseUrl;?>/themes/skote/assets/css/app.min.css")
+                                document.getElementById("app-style").setAttribute("href", "<?= $baseUrl;?>/themes/skote/assets/css/app.min.css");
+                            document.documentElement.setAttribute("data-bs-theme", "light");
+                            break;
+                        case "dark-mode-switch":
+                            document.documentElement.removeAttribute("dir");
+                            if (document.getElementById("bootstrap-style").getAttribute("href") != "<?= $baseUrl;?>/themes/skote/assets/css/bootstrap.min.css")
+                                document.getElementById("bootstrap-style").setAttribute("href", "<?= $baseUrl;?>/themes/skote/assets/css/bootstrap.min.css");
+                            if (document.getElementById("app-style").getAttribute("href") != "<?= $baseUrl;?>/themes/skote/assets/css/app.min.css")
+                                document.getElementById("app-style").setAttribute("href", "<?= $baseUrl;?>/themes/skote/assets/css/app.min.css");
+                            document.documentElement.setAttribute("data-bs-theme", "dark");
+                            break;
+                        case "rtl-mode-switch":
+                            if (document.getElementById("bootstrap-style").getAttribute("href") != "<?= $baseUrl;?>/themes/skote/assets/css/bootstrap-rtl.min.css")
+                                document.getElementById("bootstrap-style").setAttribute("href", "<?= $baseUrl;?>/themes/skote/assets/css/bootstrap-rtl.min.css");
+                            if (document.getElementById("app-style").getAttribute("href") != "<?= $baseUrl;?>/themes/skote/assets/css/app-rtl.min.css")
+                                document.getElementById("app-style").setAttribute("href", "<?= $baseUrl;?>/themes/skote/assets/css/app-rtl.min.css");
+                            document.documentElement.setAttribute("dir", "rtl");
+                            document.documentElement.setAttribute("data-bs-theme", "light");
+                            break;
+                        case "dark-rtl-mode-switch":
+                            if (document.getElementById("bootstrap-style").getAttribute("href") != "<?= $baseUrl;?>/themes/skote/assets/css/bootstrap-rtl.min.css")
+                                document.getElementById("bootstrap-style").setAttribute("href", "<?= $baseUrl;?>/themes/skote/assets/css/bootstrap-rtl.min.css");
+                            if (document.getElementById("app-style").getAttribute("href") != "<?= $baseUrl;?>/themes/skote/assets/css/app-rtl.min.css")
+                                document.getElementById("app-style").setAttribute("href", "<?= $baseUrl;?>/themes/skote/assets/css/app-rtl.min.css");
+                            document.documentElement.setAttribute("dir", "rtl");
+                            document.documentElement.setAttribute("data-bs-theme", "dark");
+                            break;
+                        default:
+                            console.log("Something wrong with the layout mode.");
+                    }
+                }
+            }
+        }(window.jQuery);
+    </script>
+    <?php $this->head() ?>
 </head>
-<body>
-
+<body data-sidebar="dark">
 <?php $this->beginBody() ?>
 
-<?= $content ?>
+    <?= $content ?>
 
 <?php $this->endBody() ?>
 </body>
